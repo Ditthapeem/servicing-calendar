9678f19d5ddb16078986a7c7886f47924f2c4eee
unknown <lakagul.peem@gmail.com>
2022-07-20 23:15:25 +0700

    Improve security

--- backend/servicing_calendar/servicing_calendar/settings.py
+++ backend/servicing_calendar/servicing_calendar/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tcnwa@ofpe9^6ry#_3%zb)mtljy7+a$0q99*quzqy*vb49v7(-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
