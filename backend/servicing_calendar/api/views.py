fd35976698c5b71ab47fce2727ccc6cd392d8094
unknown <lakagul.peem@gmail.com>
2022-07-21 22:12:15 +0700

    Create getRoutes() for API endpoint

--- backend/servicing_calendar/api/views.py
+++ backend/servicing_calendar/api/views.py
@@ -1,3 +1,15 @@
+import re
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getRoutes(request):
+    """
+    Specifide the end point of each path.
+
+    Args:
+        request: The request from web page.
+
+    Returns:
+        JsonResponse with list of end point.
+    """
+    return JsonResponse("Our API", safe=False)
