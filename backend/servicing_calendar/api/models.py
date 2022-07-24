72cf3d205fedeb51e4a40f9e8617fee065fe694e
unknown <lakagul.peem@gmail.com>
2022-07-24 23:12:47 +0700

    Edit field in model.

--- backend/servicing_calendar/api/models.py
+++ backend/servicing_calendar/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser
+import datetime
 
 class Customer(AbstractBaseUser):
     """Customer model."""
@@ -10,24 +11,24 @@ class Customer(AbstractBaseUser):
     email = models.EmailField(null=False, unique=True)
     address = models.TextField(max_length=255)
     note = models.TextField(blank=True) 
-    course_min = models.IntegerField(null=False)
+    course_minutes = models.IntegerField(null=False)
 
     USERNAME_FIELD = "username"
 
 class Reservation(models.Model):
     """User reservation model."""
 
-    class Time(models.IntegerChoices):
+    class Time(datetime.time, models.Choices):
         """Reservation time that user can reserve."""
 
-        MINIMUM = 60
-        MEDIUM = 75
-        MAXIMUM = 90
+        MINIMUM = 1, 0, 0, '60 minutes'
+        MEDIUM = 1, 15, 0, '75 minutes'
+        MAXIMUM = 1, 30, 0, '90 minutes'
 
     customer = models.ForeignKey(Customer, related_name="Reservation", on_delete=models.CASCADE)
     start = models.DateTimeField(null=False)
     end = models.DateTimeField(null=False)
-    duration = models.IntegerField(choices=Time.choices)
+    duration = models.TimeField(choices=Time.choices)
     note = models.TextField(blank=True)
 
     def __str__(self) -> str:
@@ -48,5 +49,5 @@ class ManageReservation(models.Model):
     """Manage service date."""
 
     owner = models.ForeignKey(User,related_name="Manage_Reservation", on_delete=models.CASCADE)
-    close_date = models.DateTimeField(null=False, unique=True)
+    close_date = models.DateField(null=False, unique=True)
 
