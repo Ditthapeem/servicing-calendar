b7d895b74135f3e485f5d0fd1f1385b0daaf4e9c
unknown <lakagul.peem@gmail.com>
2022-07-24 23:13:35 +0700

    Create all model serializers.

--- backend/servicing_calendar/api/serializers.py
+++ backend/servicing_calendar/api/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Reservation
+from .models import Reservation, Customer, Store, ManageReservation
+
+class CustomerSerializer(ModelSerializer):
+    """Serializer of customer model."""
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
 
 
-class NoteSerializer(ModelSerializer):
+class ReservationSerializer(ModelSerializer):
+    """Serializer of reservation model."""
+
     class Meta:
         model = Reservation
+        fields = '__all__'
+
+class StoreSerializer(ModelSerializer):
+    """Serializer of store model."""
+
+    class Meta:
+        model = Store
+        fields = '__all__'
+
+class ManageReservationSerializer(ModelSerializer):
+    """Serializer of management reservation model."""
+
+    class Meta:
+        model = ManageReservation
         fields = '__all__'
