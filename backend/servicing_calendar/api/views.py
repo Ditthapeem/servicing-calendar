0227aa142cba0469fa450141ae2a3d1680091ada
unknown <lakagul.peem@gmail.com>
2022-07-25 22:59:25 +0700

    Create booking method for reservation

--- backend/servicing_calendar/api/views.py
+++ backend/servicing_calendar/api/views.py
@@ -47,11 +47,14 @@ def get_my_calendar(request, customer_username):
     Return all reservation data of a particular username. 
 
     Args:
-        request: GET all reservation data of a particular username.
-        customer_username: The username of customer.
+        request: 
+            GET:     all reservation data of a particular username.
+            customer_username:  The username of customer.
 
     Returns:
-        GET: Response all reservation data of a particular username.
+        GET:    Response all reservation data of a particular username.
+        GET:    Response "User doesn't exist." in case of the 
+                customer_username doesn't exist.
     """
     if request.method == 'GET':
         try:
@@ -62,24 +65,32 @@ def get_my_calendar(request, customer_username):
         except:
             return Response("User doesn't exist.")
 
+@api_view(['POST'])
+def delete_booking(request, customer_username, booking_id):
+    pass
+
 @api_view(['GET', 'POST'])
-def booking(request):
+def booking(request, customer_username):
     """
+    TODO: 1. end = data["end"] should fill by backend create function() 
     Return and get data from booking page 
 
     Args:
-        request: GET all data of other reservated.
-                 POST data from customer for new reservation. 
+        request: GET:   all data of other reservated and particular customer username.
+                 POST:  data from particular customer for new reservation. 
 
     Returns:
-        GET: Response all reserved data from all customer.
-        POST: Response sepecific data from customer for new reservation.
-        POST: Fail response if customer reserve on invalid either date or time. 
+        GET:    Response all reserved data from all customer and data from particular 
+                customer username.
+        POST:   Response sepecific data from customer for new reservation.
+        POST:   Fail response if customer reserve on invalid either date or time. 
     """
     if request.method == 'GET':
+        customer_object = Customer.objects.get(username=customer_username)
+        customer_serializer = CustomerSerializer(customer_object, many=False)
         already_reserved = Reservation.objects.all()
-        serializer = ReservationSerializer(already_reserved, many=True)
-        return Response(serializer.data)
+        reservation_serializer = ReservationSerializer(already_reserved, many=True)
+        return Response([customer_serializer.data, reservation_serializer.data])
     elif request.method == 'POST':
         data = request.data
         if is_reservation_valid(data):
@@ -94,4 +105,8 @@ def booking(request):
             return Response(serializer.data)
         else:
             return Response("Fail to reserved.")
+
+@api_view(['GET'])
+def get_booking(request, customer_username, date, course):
+    pass
 
