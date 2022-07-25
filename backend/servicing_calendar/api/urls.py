2535dc4b480adb8cd83f11e73e392687c599679f
unknown <lakagul.peem@gmail.com>
2022-07-25 23:00:49 +0700

    Adding pathr for delete, booking and get_booking

--- backend/servicing_calendar/api/urls.py
+++ backend/servicing_calendar/api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
-    path('<str:customer_username>/calendar/', views.get_my_calendar, name='calendar'),
-    path('booking/', views.booking, name="booking")
+    path('<str:customer_username>/calendar/', views.get_my_calendar, name='calendar'), # get customer data
+    path('<str:customer_username>/calendar/<int:booking_id>', views.delete_booking, name='calendar'), # post delet reservation
+    path('<str:customer_username>/booking/', views.booking, name="booking"), # get aviable date and customor hours. & post new reservation.
+    path('<str:customer_username>/booking?date=<str:date>&course=<int:course>', views.get_booking, name="booking") # get available time.
 ]
