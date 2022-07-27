dd7e3cebc853f365f8726fc1e1342023d3a1d906
unknown <lakagul.peem@gmail.com>
2022-07-27 17:03:41 +0700

    Adding new urls

--- backend/servicing_calendar/api/urls.py
+++ backend/servicing_calendar/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('<str:customer_username>/calendar/', views.get_my_calendar, name='calendar'), # get customer data
-    path('<str:customer_username>/calendar/<int:booking_id>', views.delete_booking, name='calendar'), # post delet reservation
+    path('<str:customer_username>/calendar/cancel', views.delete_booking, name='cancel'), # post delet reservation
     path('<str:customer_username>/booking/', views.booking, name="booking"), # get aviable date and customor hours. & post new reservation.
-    path('<str:customer_username>/booking?date=<str:date>&course=<int:course>', views.get_booking, name="booking") # get available time.
+    path('<str:customer_username>/booking/date=<str:date>/course=<int:course>', views.get_time_booking, name="get_booking") # get available time.
 ]
